0b73e8007efb8b3e90a0d09522783cbe6e9aef67
Taavi Meinberg <TaaviMeinberg@users.noreply.github.com>
2017-09-14 10:47:08 +0300

    Lisatud on esimene PHP fail

---
+++ esimene.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Taavi Meinberg</title>
+</head>
+
+<body> 
+	<h1>Taavi Meinberg</h1>
+	<?php echo "see on test"; ?>
+</body>
+</html>
