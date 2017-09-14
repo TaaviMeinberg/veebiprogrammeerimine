5908ec9a6eff156858218a2c78ee4a450619004d
Taavi Meinberg <TaaviMeinberg@users.noreply.github.com>
2017-09-14 11:30:47 +0300

    lisatud muutujad ning if lause

--- esimene.php
+++ esimene.php
@@ -1,3 +1,23 @@
+<?php 
+	//Muutujad
+	$time = date("H:i:s");
+	$myName = "Taavi";
+	$myFamilyName = "Meinberg";
+	
+	//Hindan päeva osa
+	$hourNow = date("H");
+	$partOfDay ="";
+	if ($hourNow < 8){
+		$partOfDay = "varahommik";
+	} 
+	if ($hourNow >= 8 and $hourNow < 16) {
+		$partOfDay = "koolipäev";
+	} else {
+		$partOfDay = "vaba aeg";
+	}
+	//echo $partOfDay;
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,7 +26,12 @@
 </head>
 
 <body> 
-	<h1>Taavi Meinberg</h1>
-	<?php echo "see on test"; ?>
+	<h1><?php echo $myName . " " . $myFamilyName; ?>, veebiprogemine</h1>
+	
+	
+	<?php 
+	echo "<p>Praegu on: " . $partOfDay . "</p>";
+	echo "<p>Täna on: " . date("d-m-Y") . " Kell oli avamise hetkel: " . $time .". </p>";
+	?>
 </body>
 </html>
